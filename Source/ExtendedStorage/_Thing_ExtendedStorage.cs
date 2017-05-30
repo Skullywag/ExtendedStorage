b00a52415a6354fadd6d5298e5c31db1c498d863
Florian Maunier <fmaunier@gmail.com>
2017-05-30 21:47:09 +0200

    Initial attempt at fixing this for A17

--- Source/ExtendedStorage/_Thing_ExtendedStorage.cs
+++ Source/ExtendedStorage/_Thing_ExtendedStorage.cs
@@ -37,11 +37,11 @@ namespace ExtendedStorage
             mi_Notify_NoZoneOverlapThingSpawned.Invoke(map.zoneManager, new object[] { thing });
         }
 
-        internal static MethodInfo mi_Notify_BarrierSpawned;
-        internal static void rNotify_BarrierSpawned(Thing thing, Map map)
+        internal static MethodInfo mi_Notify_ThingAffectingRegionsSpawned;
+        internal static void rNotify_ThingAffectingRegionsSpawned(Thing thing, Map map)
         {
-            mi_Notify_BarrierSpawned = typeof(RegionDirtyer).GetMethod("Notify_BarrierSpawned", (BindingFlags)60); // public+nonpublic+instance+static
-            mi_Notify_BarrierSpawned.Invoke(map.regionDirtyer, new object[] { thing });
+            mi_Notify_ThingAffectingRegionsSpawned = typeof(RegionDirtyer).GetMethod("Notify_ThingAffectingRegionsSpawned", (BindingFlags)60); // public+nonpublic+instance+static
+            mi_Notify_ThingAffectingRegionsSpawned.Invoke(map.regionDirtyer, new object[] { thing });
         }
 
         internal void _SpawnSetup(Map map)
@@ -50,11 +50,11 @@ namespace ExtendedStorage
             {
                 Log.Error(string.Concat(new object[]
                 {
-            "Spawning destroyed thing ",
-            this,
-            " at ",
-            this.Position,
-            ". Correcting."
+                    "Spawning destroyed thing ",
+                    this,
+                    " at ",
+                    this.Position,
+                    ". Correcting."
                 }));
                 SetMapIndexOrState(this, -1);
                 if (this.HitPoints <= 0 && this.def.useHitPoints)
@@ -66,10 +66,10 @@ namespace ExtendedStorage
             {
                 Log.Error(string.Concat(new object[]
                 {
-            "Tried to spawn already-spawned thing ",
-            this,
-            " at ",
-            this.Position
+                    "Tried to spawn already-spawned thing ",
+                    this,
+                    " at ",
+                    this.Position
                 }));
                 return;
             }
@@ -79,76 +79,70 @@ namespace ExtendedStorage
                 Log.Error("Tried to spawn thing " + this + ", but the map provided does not exist.");
                 return;
             }
-            this.holdingContainer = null;
             SetMapIndexOrState(this, (sbyte)num);
-            this.Map.listerThings.Add(this);
+            RegionListersUpdater.RegisterInRegions(this, map);
+			if (!map.spawnedThings.TryAdd(this, false))
+			{
+				Log.Error("Couldn't add thing " + this + " to spawned things.");
+			}
+			map.listerThings.Add(this);
+			map.thingGrid.Register(this);
             if (Find.TickManager != null)
             {
                 Find.TickManager.RegisterAllTickabilityFor(this);
             }
-            if (this.def.drawerType != DrawerType.RealtimeOnly)
-            {
-                CellRect.CellRectIterator iterator = this.OccupiedRect().GetIterator();
-                while (!iterator.Done())
-                {
-                    this.Map.mapDrawer.MapMeshDirty(iterator.Current, MapMeshFlag.Things);
-                    iterator.MoveNext();
-                }
-            }
-            if (this.def.drawerType != DrawerType.MapMeshOnly)
-            {
-                this.Map.dynamicDrawManager.RegisterDrawable(this);
-            }
-            if (this.def.hasTooltip)
-            {
-                this.Map.tooltipGiverList.RegisterTooltipGiver(this);
-            }
+            this.DirtyMapMesh(map);
+			if (this.def.drawerType != DrawerType.MapMeshOnly)
+			{
+				map.dynamicDrawManager.RegisterDrawable(this);
+			}
+            map.tooltipGiverList.Notify_ThingSpawned(this);
             if (this.def.graphicData != null && this.def.graphicData.Linked)
             {
-                this.Map.linkGrid.Notify_LinkerCreatedOrDestroyed(this);
-                this.Map.mapDrawer.MapMeshDirty(this.Position, MapMeshFlag.Things, true, false);
+                map.linkGrid.Notify_LinkerCreatedOrDestroyed(this);
+                map.mapDrawer.MapMeshDirty(this.Position, MapMeshFlag.Things, true, false);
             }
             if (!this.def.CanOverlapZones)
             {
                 rNotify_NoZoneOverlapThingSpawned(this, map);
             }
-            if (this.def.regionBarrier)
-            {
-                rNotify_BarrierSpawned(this, map);
-            }
+            if (this.def.AffectsRegions)
+			{
+				rNotify_ThingAffectingRegionsSpawned(this, map);
+			}
             if (this.def.pathCost != 0 || this.def.passability == Traversability.Impassable)
             {
-                this.Map.pathGrid.RecalculatePerceivedPathCostUnderThing(this);
+                map.pathGrid.RecalculatePerceivedPathCostUnderThing(this);
             }
             if (this.def.passability == Traversability.Impassable)
             {
-                this.Map.reachability.ClearCache();
+                map.reachability.ClearCache();
             }
-            this.Map.coverGrid.Register(this);
+            map.coverGrid.Register(this);
             if (this.def.category == ThingCategory.Item)
             {
-                this.Map.listerHaulables.Notify_Spawned(this);
+                map.listerHaulables.Notify_Spawned(this);
             }
-            this.Map.attackTargetsCache.Notify_ThingSpawned(this);
-            Region validRegionAt_NoRebuild = this.Map.regionGrid.GetValidRegionAt_NoRebuild(this.Position);
+            map.attackTargetsCache.Notify_ThingSpawned(this);
+            Region validRegionAt_NoRebuild = map.regionGrid.GetValidRegionAt_NoRebuild(this.Position);
             Room room = (validRegionAt_NoRebuild != null) ? validRegionAt_NoRebuild.Room : null;
             if (room != null)
             {
                 room.Notify_ContainedThingSpawnedOrDespawned(this);
             }
-            if (this.def.category == ThingCategory.Item)
-            {
-                Building_Door building_Door = this.Position.GetEdifice(this.Map) as Building_Door;
-                if (building_Door != null)
-                {
-                    building_Door.Notify_ItemSpawnedOrDespawnedOnTop(this);
-                }
-            }
             StealAIDebugDrawer.Notify_ThingChanged(this);
-            if (this is IThingContainerOwner && Find.ColonistBar != null)
-            {
-                Find.ColonistBar.MarkColonistsDirty();
-            }
+			if (this is IThingHolder && Find.ColonistBar != null)
+			{
+				Find.ColonistBar.MarkColonistsDirty();
+			}
+			if (this.def.category == ThingCategory.Item)
+			{
+				SlotGroup slotGroup = this.Position.GetSlotGroup(map);
+				if (slotGroup != null && slotGroup.parent != null)
+				{
+					slotGroup.parent.Notify_ReceivedThing(this);
+				}
+			}
         }
     }
 }

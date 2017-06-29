21b3a8ccb950c1686810eeba9538759b42b2e91a
DoctorVanGogh <DoctorVanGogh@users.noreply.github.com>
2017-06-29 23:16:05 +0200

    Switched from a complete override of ITab_Storage.FillTab to an absolute minimal IL patch - enables playing nice with the neighbours

---
+++ Source/ExtendedStorage/Patches/ITab_Storage_FillTab.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using Harmony;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace ExtendedStorage
+{
+    [HarmonyPatch(typeof(RimWorld.ITab_Storage), "FillTab")]
+    public class ITab_Storage_FillTab
+    {
+        private static readonly PropertyInfo piSelStoreSettingsParent;
+
+        static ITab_Storage_FillTab()
+        {
+            // accessor for private field
+            piSelStoreSettingsParent = typeof(RimWorld.ITab_Storage)
+                .GetProperty("SelStoreSettingsParent", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        private static IStoreSettingsParent GetSelStoreSettingsParent(RimWorld.ITab_Storage tab)
+        {
+            return (IStoreSettingsParent) piSelStoreSettingsParent.GetValue(tab, null);
+        }
+
+
+        /// <summary>
+        /// Wrapper that get's the <see cref="IStoreSettingsParent"/> &amp; <see cref="StorageSettings"/> for 
+        /// a <see cref="RimWorld.ITab_Storage"/> <paramref name="tab"/>.
+        /// 
+        /// Special cases ExtendedStorage's <see cref="ExtendedStorage.ITab_Storage"/> tabs.
+        /// </summary>
+        /// <remarks>
+        /// Wrapper has been constructed somewhat convoluted to minimize IL-byte size in invocation. 
+        /// 
+        /// A tupled return value (instead of using out parameters) would be cleaner, but that would necessitate
+        /// retrievals from properties/fields afterwards (which would mean more bytes of IL).
+        /// 
+        /// Also the <paramref name="parent"/> parameter is as many <c>out</c> uses as are possible, since
+        /// the <see cref="StorageSettings"/> is actually wrapped inside an anonymous compiler generated type
+        /// (which is inaccessible to us).
+        /// </remarks>
+        public static StorageSettings GetSettings(RimWorld.ITab_Storage tab, out IStoreSettingsParent parent) {
+            ExtendedStorage.ITab_Storage extended = tab as ExtendedStorage.ITab_Storage;
+
+            if (extended == null) {
+                parent = GetSelStoreSettingsParent(tab);
+               return parent.GetStoreSettings();
+            } else {
+                Building_ExtendedStorage building = extended.Building;
+
+                parent = building;
+                return building?.userSettings;
+            }
+        }
+
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr, ILGenerator ilgen)
+        {
+            /*  Don't completely replace the base implementation. Only change the absolutely bare minimum.
+             *  
+             *  For us, this is a different value of the <c>IStoreSettingsParent</c> & <c>StorageSettings</c> local
+             *  variable inside the base implementation.
+             *  
+             *  So use a wrapper (<see cref="GetSettings" />) that incorporates the base retrievel for those locals and 
+             *  special cases our type.
+             * 
+             */
+
+            List<CodeInstruction> instructions = new List<CodeInstruction>(instr);
+
+            /*      IL Changes:
+             
+			                newobj		instance void RimWorld.ITab_Storage/'<FillTab>c__AnonStorey452'::.ctor()
+	                +1	>>>	dup			# we could do stloc+ldloc, but dup+stloc is shorter ;)
+			                stloc.s		8
+			                ldarg.0
+	                -5	<<<	call		instance class RimWorld.IStoreSettingsParent RimWorld.ITab_Storage::get_SelStoreSettingsParent()
+	                +2	>>>	ldloca.s	0
+	                +5	>>>	call		class ['Assembly-CSharp']RimWorld.StorageSettings ExtendedStorage.ITab_Storage_FillTab::GetSettings(class ['Assembly-CSharp']RimWorld.ITab_Storage, class ['Assembly-CSharp']RimWorld.IStoreSettingsParent&)
+	                -1	<<<	stloc.0
+	                -2	<<<	ldloc.s		8
+	                -1	<<<	ldloc.0
+	                -5	<<<	callvirt	instance class RimWorld.StorageSettings RimWorld.IStoreSettingsParent::GetStoreSettings()
+	                +1	>>>	nop         # restore byte length
+	                +1	>>>	nop         # restore byte length
+	                +1	>>>	nop         # restore byte length
+	                +1	>>>	nop         # restore byte length
+	                +1	>>>	nop         # restore byte length
+	                +1	>>>	nop         # restore byte length
+                            stfld 		class RimWorld.StorageSettings RimWorld.ITab_Storage/'<FillTab>c__AnonStorey452'::settings
+                            ...
+            */
+            instructions.Insert(1, new CodeInstruction(OpCodes.Dup));
+            instructions.RemoveAt(4);
+            instructions.Insert(4, new CodeInstruction(OpCodes.Ldloca_S, 0));
+            instructions.Insert(5, new CodeInstruction(OpCodes.Call, typeof(ITab_Storage_FillTab).GetMethod(nameof(ITab_Storage_FillTab.GetSettings))));
+            instructions.RemoveAt(6);
+            instructions.RemoveAt(6);
+            instructions.RemoveAt(6);
+            instructions.RemoveAt(6);
+            for (int i=1; i<=6; i++)
+                instructions.Insert(6, new CodeInstruction(OpCodes.Nop));
+
+            return instructions;
+        }
+    }
+}

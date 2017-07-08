e45dc88682996b60d1ace2c19ae4961934e3338f
DoctorVanGogh <DoctorVanGogh@users.noreply.github.com>
2017-07-08 11:42:37 +0200

    More merge blunders

--- Source/ExtendedStorage/Patches/Thing_SpawnSetup.cs
+++ Source/ExtendedStorage/Patches/Thing_SpawnSetup.cs
@@ -15,7 +15,7 @@ namespace ExtendedStorage
 {
 
     [HarmonyPatch(typeof(Thing), nameof(Thing.SpawnSetup))]
-    class Patches
+    class Thing_SpawnSetup
     {
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
@@ -29,7 +29,7 @@ namespace ExtendedStorage
                 {
                     yield return new CodeInstruction(OpCodes.Ldarg_0);
                     yield return new CodeInstruction(OpCodes.Ldarg_1);
-                    yield return new CodeInstruction(OpCodes.Call, typeof(Patches).GetMethod(nameof(VerifyThingShouldNotBeTruncated)));
+                    yield return new CodeInstruction(OpCodes.Call, typeof(Thing_SpawnSetup).GetMethod(nameof(VerifyThingShouldNotBeTruncated)));
                     yield return new CodeInstruction(OpCodes.Brtrue, instruction.operand);
                 }
             }

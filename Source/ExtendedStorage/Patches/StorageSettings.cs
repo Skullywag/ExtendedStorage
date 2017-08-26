8d395ca3031020c90ba04c50c60fa03877a67c9d
DoctorVanGogh <DoctorVanGogh@users.noreply.github.com>
2017-08-26 16:25:00 +0200

    bugfix: usersettings not propagating values after a load

--- Source/ExtendedStorage/Patches/StorageSettings.cs
+++ Source/ExtendedStorage/Patches/StorageSettings.cs
@@ -6,25 +6,6 @@ using RimWorld;
 using Verse;
 
 namespace ExtendedStorage {
-    [HarmonyPatch(typeof(StorageSettings), methodTryNotifyChanged)]
-    class StorageSettings_TryNotifyChanged
-    {
-
-        public const string methodTryNotifyChanged = "TryNotifyChanged";
-
-        public static bool Prefix(StorageSettings __instance)
-        {
-            var us = __instance as UserSettings;
-
-            if (us != null) {
-                us.NotifyOwnerSettingsChanged();
-                return false;
-            }
-            return true;
-        }
-    }
-
-
     [HarmonyPatch(typeof(StorageSettings), "set_" + nameof(StorageSettings.Priority))]
     class StorageSettings_set_Priority
     {
--- Source/ExtendedStorage/UserSettings.cs
+++ Source/ExtendedStorage/UserSettings.cs
@@ -6,12 +6,26 @@ using RimWorld;
 using Verse;
 
 namespace ExtendedStorage {
-    public class UserSettings : StorageSettings {
+    public class UserSettings : StorageSettings, IExposable
+    {
+
+        private static Action<ThingFilter, Action> setThingFilterSettingsChangedCallback = Access.GetFieldSetter<ThingFilter, Action>("settingsChangedCallback");
+
+
         private IUserSettingsOwner _owner;
 
         public UserSettings(IUserSettingsOwner owner) : base(owner)
         {
             _owner = owner;
+
+            // needed for 'newly constructed' ES buildings
+            FixupFilterChangeCallback();
+        }
+
+        private void FixupFilterChangeCallback()
+        {
+            // HACK: reflection instanced members seem to get shit inlined, which means out harmony patches dont apply, which means we're fucked...
+            setThingFilterSettingsChangedCallback(this.filter, NotifyOwnerSettingsChanged);
         }
 
 
@@ -20,5 +34,13 @@ namespace ExtendedStorage {
             _owner.Notify_UserSettingsChanged();
         }
 
+        void IExposable.ExposeData() {
+            base.ExposeData();
+
+            if (Scribe.mode == LoadSaveMode.PostLoadInit) {
+                // needed for ES buildings loaded from saves...
+                FixupFilterChangeCallback();
+            }
+        }
     }
 }
